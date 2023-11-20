0e4867e95cc6c7d20edf825884d956ae8f23648f
erica-210 <137020409+erica-210@users.noreply.github.com>
2023-11-19 20:05:16 -0600

    created crud for routes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# E-commerce Back End Starter Code
+# E-commerce Back End 
+
+https://sequelize.org/docs/v6/
+https://stackoverflow.com/
--- models/index.js
+++ models/index.js
@@ -18,6 +18,7 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   // Defines the third table needed to store the foreign keys
+  foreignKey: 'product_id',
   through: {
     model: ProductTag,
     unique: false
@@ -27,6 +28,7 @@ Product.belongsToMany(Tag, {
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
   through: {
     model: ProductTag,
     unique: false
